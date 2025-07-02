c732f2c6c18267e10bbeafc32d3da614863a0851
kanayan1235 <kininaru5555@gmail.com>
2025-07-03 00:09:59 +0900

    Delete backend/slime_logic.py

--- backend/slime_logic.py
+++
@@ -1,9 +0,0 @@
-
-from PIL import Image, ImageEnhance
-
-def process_image(input_path, output_path):
-    img = Image.open(input_path).convert("RGBA")
-    # 仮処理（輝度アップ）
-    enhancer = ImageEnhance.Brightness(img)
-    result = enhancer.enhance(1.2)
-    result.save(output_path)
