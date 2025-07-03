4cbd635d0464da05a5c3c89a5b9bcf5e433275fc
kanayan1235 <kininaru5555@gmail.com>
2025-07-03 20:03:14 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,10 +17,13 @@ load_dotenv()
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET")
-)
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"))
 
 app = FastAPI()
+
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # パス設定
 UPLOAD_DIR = "backend/uploaded"
